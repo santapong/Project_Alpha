e6bf655af31bae35cbfd47a8af9b1232565d9838
Santapong <santapongsondhi@gmail.com>
2025-03-13 18:23:23 +0700

    Update Google Oauth2

--- src/api/router/login.py
+++ src/api/router/login.py
@@ -10,6 +10,11 @@ from fastapi import Depends
 
 import jwt
 
+# NOTE: Need to connect with Database.
+# FIXME: POC for Security.
+# FIXME: POC Starlette.
+
+
 load_dotenv()
 # Parsing .ENV
 OAUTH_PROVIDER=os.getenv("OAUTH2_PROVIDER")
@@ -28,11 +33,13 @@ router_login = APIRouter(
     prefix=path_prefix, 
 )
 
-#FIXME: Redirect Error
+# Redirect to google Oauth.
 @router_login.get("/google")
 async def login_google():
     return RedirectResponse(url=f"https://accounts.google.com/o/oauth2/auth?response_type=code&client_id={CLIENT_ID}&redirect_uri={REDIRECT_URI}&scope=openid%20profile%20email&access_type=offline")
     
+    
+# Get token from google redirect
 @router_login.get("/auth/google")
 async def auth_google(code: str):
     token_url = "https://accounts.google.com/o/oauth2/token"
@@ -46,8 +53,11 @@ async def auth_google(code: str):
     response = requests.post(token_url, data=data)
     access_token = response.json().get("access_token")
     user_info = requests.get("https://www.googleapis.com/oauth2/v1/userinfo", headers={"Authorization": f"Bearer {access_token}"})
+    
     return user_info.json()
 
+
+# Token from FastAPI
 @router_login.get("/token")
 async def get_token(token: str = Depends(oauth2_scheme)):
     return jwt.decode(token, CLIENT_SECRET, algorithms=["HS256"])
