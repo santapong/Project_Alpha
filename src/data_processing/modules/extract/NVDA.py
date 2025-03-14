43f36a7908ef5dfd0a7eb36f83605e3fa4f3d530
Santapong <santapongsondhi@gmail.com>
2025-03-14 23:55:59 +0700

    Fix NVDA Data Type

--- src/data_processing/modules/extract/NVDA.py
+++ src/data_processing/modules/extract/NVDA.py
@@ -3,6 +3,7 @@ from dagster import asset, Output, AssetIn
 from dagster import AssetExecutionContext
 
 from pyspark.sql import SparkSession
+from pyspark.sql.types import FloatType, StructType, DateType, StructField
 
 kinds={"spark"}
 
@@ -15,10 +16,19 @@ def extract_data(pyspark: PySparkResource):
     
     file_path = "data/kaggle/NVDA.csv"
     
+    schema = StructType([
+        StructField("Date", DateType()),
+        StructField("Open", FloatType()),
+        StructField("High", FloatType()),
+        StructField("Low", FloatType()),
+        StructField("Close", FloatType()),
+        StructField("Volume", FloatType()),
+    ])
+    
     # Read CSV from fliepath.
     # TODO: Make it can Parsing file. 
     spark_session: SparkSession = pyspark.spark_session
-    df = spark_session.read.csv(path=file_path)
+    df = spark_session.read.csv(path=file_path, schema=schema, header=True)
     
     # Save file as a parquet. 
     output_path = "data/parquet/NVDA.parquet"
@@ -39,6 +49,7 @@ def load_data(context: AssetExecutionContext, NVDA_path, pyspark: PySparkResourc
     spark_session: SparkSession = pyspark.spark_session
     df = spark_session.read.parquet(NVDA_path)
     
+    # Load Data to Postgresql.
     df.write \
         .format("jdbc") \
         .option("url", "jdbc:postgresql://localhost:5432/postgres") \
